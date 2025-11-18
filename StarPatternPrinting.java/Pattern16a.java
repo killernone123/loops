6d3f34ab00ce10f32c7c34b2285677550f31357a
killernone123 <piyushphoolmali8@gmail.com>
2025-11-18 13:17:45 +0530

    pa

---
+++ StarPatternPrinting.java/Pattern16a.java
@@ -0,0 +1,17 @@
+
+
+public class Pattern16a {
+    public static void main(String[] args) {
+        int n = 9;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || j == (n / 2 + 1)) {
+                    System.out.print(" * ");
+                }else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
